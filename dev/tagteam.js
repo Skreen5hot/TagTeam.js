32630e1c0d86fc1090820420c9e3100999518d66
Skreen5hot <Skreen5hot@users.noreply.github.com>
2026-03-06 13:01:37 +0000

    deploy: b50864ebff4c2d64911ff6d1c7e5f993571560c1

--- dev/tagteam.js
+++ dev/tagteam.js
@@ -325944,7 +325944,7 @@ class SemanticGraphBuilder {
      * Version information
      */
     version: '4.0.0',
-    BUILD: 'build 268 | 3d8b245 | 2026-03-06T11:49:39.060Z',
+    BUILD: 'build 268 | b50864e | 2026-03-06T13:01:35.206Z',
 
     // Advanced: Expose classes for power users
     SemanticRoleExtractor: SemanticRoleExtractor,
